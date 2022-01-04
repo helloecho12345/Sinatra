33fe0e2244749fb2ece4ab8896c63aaae822c64d
E Cho <helloecho12345@gmail.com>
2022-01-04 16:02:19 +0000

    Sinatra Start and Restart - load secret path

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -1,1 +1,19 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
+
+=begin
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+=end
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "This is a secret"
+end
+
+#end
