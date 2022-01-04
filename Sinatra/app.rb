d9fcd63f2813dbc138a2e4b6fe8cde0d0f2c58cd
E Cho <helloecho12345@gmail.com>
2022-01-04 18:00:07 +0000

    Sinatra 12 : Introducing Params - added if statement to index.erb

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -16,8 +16,14 @@ get '/secret' do
   "This is a secret"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name=["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name=params[:name]
   erb(:index)
 end
 
