19d8712561fb7af88bc877ac8e263f1757592780
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-05 17:03:18 +0000

    Add post route to named cat, add get route for cat form

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -21,10 +21,13 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name=params[:name]
   erb(:index)
 end
 
+get '/named-cat-form' do
+  erb(:form)
+end
 
